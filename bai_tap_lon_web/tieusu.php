b4d48ced3680a5b1b8dcb303414977abdd269fb3
trandinhthang14 <tholac113@gmail.com>
2022-12-07 22:43:14 +0700

    push-code

---
+++ bai_tap_lon_web/tieusu.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Phủ Dầy</title>
+
+    <link rel="stylesheet" href="c.css" />
+    <link rel="stylesheet" href="main unes.css" />
+
+</head>
+
+<body class="home">
+    <header class="header__navbar">
+        <div class="header__navbar-list">
+            <div class="header__navbar-list-item">
+                <a href="./index.php" class="header__navbar-list-item-icon-fist">
+                    <i class="fa-solid fa-house-chimney"></i>
+                    <img src="./anh/home.png" alt="" />
+                    Trang Chủ
+                </a>
+
+                <a href="" class="header__navbar-list-item-icon-fist">
+                    <img src="./anh/gb.png" alt="" />
+                    English
+                </a>
+
+                <a href="https://www.facebook.com/phuday.vn/" class="header__navbar-list-item-icon-fist">
+                    <i class="fa-brands fa-facebook"></i>
+                    <img src="./anh/facebook.png" alt="" />
+                </a>
+
+                <a href="https://youtu.be/Tpb1vGV3RoY" class="header__navbar-list-item-icon-fist">
+                    <i class="fa-brands fa-youtube"></i>
+                    <img src="./anh/youtube2.png" alt="" />
+                </a>
+
+                <div class="header__navbar-list-item-icon--seacher">
+                    <input class="input" type="text" placeholder="Tìm Kiếm" />
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </div>
+            </div>
+
+            <div class="header__navbar-fist">
+                <a href="./index.php" class="header__navbar-list-img">
+                    <img src="./anh/logo.jpg" alt="Phủ Dầy Vụ Bản Nam Định " />
+                </a>
+            </div>
+
+            <ul class="header__navbar-item">
+                <a href="gioithieu.php">
+                    <li class="header__navbar-item--hot">Giới thiệu</li>
+                </a>
+                <a href="kinhnghiem.php">
+                    <li class="header__navbar-item--hot">Kinh nghiệm đi Phủ Dầy</li>
+                </a>
+                <li class="header__navbar-item--hot">Đăng nhập</li>
+            </ul>
+        </div>
+
+        <div class="header__navbar-second">
+            <ul class="header__navbar-second-warp">
+                <li class="header__navbar-second-list">
+                    <a class="link-white" href=""> Lễ hội Phủ Dầy </a>
+                    <ul class="header__navbar-level1">
+                        <li class="header__navbar-level1-list">
+                            <ul class="header__navbar-level1-son">
+                                <a href="sacphong.php">
+                                    <li class="header__navbar-level2">
+                                        Các sắc phong cho Thánh Mẫu Liễu Hạnh và dòng họ Trần Lê ở
+                                        Phủ Dầy
+                                    </li>
+                                </a>
+                            </ul>
+                        </li>
+
+                        <li class="header__navbar-level1-list">
+                            <ul class="header__navbar-level1-son">
+                                <a href="10ngaylehoi.php">
+                                    <li class="header__navbar-level2">
+                                        Mười ngày lễ hội Phủ Dầy
+                                    </li>
+                                </a>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="header__navbar-second-list">
+                    <a class="link-white" href=""> Tín ngưỡng thờ Mẫu </a>
+                    <ul class="header__navbar-level1">
+                        <li class="header__navbar-level1-list">
+                            <ul class="header__navbar-level1-son">
+                                <a href="tieusu.php">
+                                    <li class="header__navbar-level2">
+                                        Tiểu sử Đức Thánh Mẫu Phủ Dầy
+                                    </li>
+                                </a>
+                            </ul>
+                        </li>
+                        git config --global user.name trandinhthang14
+                        <li class="header__navbar-level1-list">
+                            <ul class="header__navbar-level1-son">
+                            <a href="nghile.php">
+                                <li class="header__navbar-level2">
+                                    Nghi lễ tôn nhang bản mệnh
+                                </li>
+                                </a> git config --global user.email tholac113@gmail.com
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="header__navbar-second-list">
+                    <a class="link-white" href=""> Chợ Viềng </a>
+                    <ul class="header__navbar-level1">
+                        <li class="header__navbar-level1-list">
+                            <ul class="header__navbar-level1-son">
+                            <a href="chovieng.php">
+                                <li class="header__navbar-level2">
+                                    Chợ Viềng Nam Định điểm đến hấp dẫn dịp đầu xuân
+                                </li>
+                                </a>
+                            </ul>
+                        </li>
+                        <li class="header__navbar-level1-list">
+                            <ul class="header__navbar-level1-son">
+                                <li class="header__navbar-level2">
+                                    Đầu xuân đi chợ Viềng mua may cầu lành
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="header__navbar-second-list">
+                    <a class="link-white" href=""> Trần Triều Hiển Thánh </a>
+                    <ul class="header__navbar-level1">
+                        <li class="header__navbar-level1-list">
+                            <ul class="header__navbar-level1-son">
+                                <li class="header__navbar-level2">
+                                    Các vị thánh trong Công đồng Trần Triều
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li class="header__navbar-level1-list">
+                            <ul class="header__navbar-level1-son">
+                                <li class="header__navbar-level2">Tiến mã lục bộ nhà Trần</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="header__navbar-second-list">
+                    <a class="link-white" href=""> Phật pháp </a>
+                    <ul class="header__navbar-level1">
+                        <li class="header__navbar-level1-list">
+                            <a href="#" class="link-white-1 ml">
+                                Một số điều cần lưu ý khi đi lễ đền chùa</a>
+                        </li>
+
+                        <li class="header__navbar-level1-list">
+                            <a href="#" class="link-white-1 ml">Chùa Tiên Hương Phủ Dầy tổ chức đại lễ Phật đản năm
+                                2022</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </header>
+    <main>
+        <div class="main-noidung" href="">
+            <h1 class="main-content--text">
+
+                <a style="margin-left:550px">
+                Tiểu sử Đức Thánh Mẫu Phủ Dầy
+                </a>
+
+            </h1>
+            <div class="medium">
+                <br>
+                <strong></strong><br>
+
+                Chép theo ngọc phả thời tiểu sử Đức Thánh Mẫu Phủ Dầy thực là giản dị không có chút gì là huyền bí cả.
+                Đức Thánh Mẫu Phủ Dầy giáng sinh đêm trăng rằm tháng 8 năm 1557 (năm Đinh Tỵ – Lê Thiên Hựu đệ nhất tức
+                đời vua Lê Anh Tông) tháng Thìn, ngày Dần, giờ Dần. Năm 1577 (năm Đinh Sửu) không bệnh mà mất.<br>
+                Thân phụ Ngài là Lê Công Chính và thân mẫu Ngài là Trần Thị Phúc.<br>
+                Cao cao Tổ thuộc dòng dõi họ Lê ở trong Thanh Hóa, ở ẩn ra ngoài làng Kẻ Dầy, đổi sang họ Trần, tới đời
+                thứ 3 thì lại đổi về họ Lê (Lê Công – Lê Đức Chính). Sau này con cháu mang dòng họ Trần Lê.<br>
+                Lê Công đặt tên Ngài là Lê Thị Thắng, tên riêng của Ngài là Giáng Tiên vì ngài đẹp như tiên giáng trần.
+                Lê Công là một nhà nho, phong lưu, sung túc, người ở xã Tiên Hương, có đủ vật lực cho con gái theo đòi
+                bút nghiêng , thi thư kinh sử, may vá thêu thùa. Nàng Giáng Tiên hồi bấy giờ thật là một người con gái
+                hoàn hảo, có nhan sắc thông minh, nết na, lại tài giỏi về nữ công, âm nhạc, thi phú.<br>
+                Những lúc nhàn rỗi, Giáng Tiên thường dạo chơi ra vườn gẩy đàn ngâm thơ. Hiện vườn ấy hãy còn di tích,
+                lại đăng sau đền thờ Mẫu ở xã Tiên Hương bây giờ.<br>
+                <br>
+                <p>
+                    <img style="margin-left:320px;width:665px;height:431px" , src="./anh/tiêusu.jpg"><br>
+                    <i style="margin-left:450px"> </i>
+                <p><br>
+                    Năm 19 tuổi thì Giáng Tiên xuất giá kết hôn cùng với Trần Đào Lang. Đào Lang là con nuôi một ông
+                    quan hưu trí họ Trần ở thôn Vân Đình cũng thuộc xã Tiên Hương. Tục truyền rằng ông quan họ Trần ấy
+                    đã nhiều tuổi mà không có con, một hôm ra dạo mát vườn hoa tìm được một cậu bé, ai đem bỏ ở gốc cây
+                    đào, bèn đem về nuôi và đặt tên là Đào Lang.<br>
+                    Nàng Giáng Tiên về nhà chồng, trên thuận dưới hòa, nức tiếng là người hiền phụ. Đước một năm sinh hạ
+                    được một giai đặt tên là Lê Nhâm, năm sau nữa lại sinh thêm được một gái tên Lê Cổn.<br>
+                    Sau khi lấy vợ, Đào Lang tỏ vẻ hơi trễ nải việc học hành. Nhân lúc canh khuya dệt vải, nàng Giáng
+                    Tiên làm một vài thơ có 28 vị tinh tú để răn đức lang quân. Đào Lang cũng họa một bài có 28 vị sao,
+                    và sau khi ấy trở nên rất chăm chỉ.<br>
+                    Giữa lúc gia đình đang sống trong cảnh yên vui, đầm ấm, trên có cha mẹ già, dưới có hai con thơ, vui
+                    vầy sum họp thì thương ôi nàng Giáng Tiên tự nhiên vô bệnh mà mất. Nàng mất năm 1577 (năm Đinh Sửu),
+                    giờ Dần ngày mùng 3 tháng 3, khi ấy mới vừa 21 tuổi, mộ táng ở xứ Cây Đa xã Tiên Hương, hiện nay
+                    lăng của ngài vẫn còn ở đấy. Năm 1938, người trong họ Đào chi và những người tín ngưỡng Đức thánh
+                    Mẫu ở trong Trung Kỳ đã hưng công đóng góp xây dựng thành một ngôi lăng mộ Đức Thánh Mẫu Phủ Dầy
+                    nguy nga tráng lệ.<br>
+            </div>
+    </main>
+    <div class="comment">
+        <button> Bình luận</button>
+    </div>
+    <div class="bottom">
+        <div class="bottom-home">
+            <section class="section vi-sao1" id="section_559935216">
+                <div class="bg section-bg fill bg-fill bg-loaded">
+                    <div class="section-bg-overlay absolute fill"></div>
+                </div>
+                <div class="section-content relative">
+                    <div class="row" id="row-163900567">
+                        <div id="col-30449085" class="col small-12 large-12">
+                            <div class="col-inner text-center">
+                                <p style="text-align: center;"><em><span style="font-size: 120%; color: #ffffff;">“Là
+                                            một người con sinh ra
+                                            và
+                                            lớn lên trên mảnh đất Nam Định , một vùng quê giàu truyền thống yêu
+                                            nước
+                                            , một trong những cái nôi sinh ra tín ngưỡng thờ Mẫu là Phủ Dầy ,
+                                            được
+                                            nuôi dưỡng trong một vùng quê giàu truyền thống trong tôi cũng một
+                                            phần
+                                            nào đó ngẫm được tư tưởng uống nước nhớ nguồn ,tôi mong muốn mang
+                                            những
+                                            giá trị văn hoá di tích Phủ Dầy đến với đông đảo người dân Việt Nam
+                                            mà
+                                            còn là giá trị văn hoá của toàn nhân loại”</span></em></p>
+                                <a href="#" target="_self" class="button primary lowercase" style="border-radius:99px;">
+                                    <i class="icon-heart"></i> <span>Liên hệ tư vấn miễn phí!</span>
+                                </a>
+                            </div>
+                        </div>
+                        <style>
+                        #row-163900567>.col>.col-inner {
+                            padding: 0px 0px 0 0px;
+                        }
+                        </style>
+                    </div>
+                </div>
+                <style>
+                #section_559935216 {
+                    padding-top: 20px;
+                    padding-bottom: 20px;
+                    background-color: crimson;
+                    background-image: url(https://phuday.com/wp-content/uploads/2021/05/bg34-1.jpg);
+                }
+                </style>
+            </section>
+
+        </div>
+    </div>
+    </div>
+
+    <footer class="footer">
+        <div class="container">
+            <div class="last">
+                <div class="last__foot" style="margin-left: 40px;">
+                    <strong class="upper">Phủ Dầy Vụ Bản Nam Định</strong>
+                    <br>
+                    Địa chỉ: Kim Thái, H.Vụ Bản, T.Nam Định
+                    <br>
+                    Điện thoại: 0902 202 202
+                    <span class=" tree "> | </span>
+
+                    Email"
+                    <a href="mailto:Phuday@gmail.vn"> Phuday@gmail.vn </a>
+                    <br>
+
+
+                </div>
+
+                <div class="last__foot  t__r ">
+                    <p>
+                        <iframe
+                            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fphuday.vn%2Fabout%2F%3Fref%3Dpage_internal&tabs=timeline&width=340&height=100px&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+                            width="340" height="100px" style="border:none;overflow:hidden" scrolling="no"
+                            frameborder="0" allowfullscreen="true"
+                            allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3740.5337370608295!2d106.07319082378179!3d20.360871872022564!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135dfda698432fd%3A0xe4e6ca9d791f57d0!2zUGjhu6cgROG6p3k!5e0!3m2!1svi!2sus!4v1669569941635!5m2!1svi!2sus"
+                            width="150" height="100" style="border:0;" allowfullscreen="" loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </p>
+
+                    <div class="last__foot-add">
+                        <div class="end">
+                            <i class="fa-solid fa-globe"></i>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+
+</body>
+
+</html>
